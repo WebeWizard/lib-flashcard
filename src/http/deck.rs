e9189780ec59a611c99cc41dafdb64167b36fd51
WebeWizard <webewizard@gmail.com>
2020-01-07 15:21:15 -0700

    add delete deck responder

--- src/http/deck.rs
+++ src/http/deck.rs
@@ -172,10 +172,65 @@ impl<'f> Responder for UpdateDeckResponder<'f> {
                 }
               }
             }
-            Err(_err) => {
-              println!("{:?}", _err);
-              return Err(400);
-            } // bad request
+            Err(_err) => return Err(400), // bad request
+          },
+          None => return Err(400),
+        },
+        Err(_err) => return Err(500),
+      },
+      None => return Err(400),
+    }
+  }
+}
+
+// DELETE DECK
+#[derive(Deserialize)]
+pub struct DeleteDeckForm {
+  #[serde(deserialize_with = "webe_auth::utility::deserialize_from_string")]
+  deck_id: u64,
+  name: String,
+}
+
+pub struct DeleteDeckResponder<'f> {
+  flash_manager: &'f FlashManager<'f>,
+}
+
+impl<'f> DeleteDeckResponder<'f> {
+  pub fn new(flash_manager: &'f FlashManager) -> DeleteDeckResponder<'f> {
+    DeleteDeckResponder {
+      flash_manager: flash_manager,
+    }
+  }
+}
+
+impl<'f> Responder for DeleteDeckResponder<'f> {
+  fn build_response(
+    &self,
+    request: &mut Request,
+    params: &HashMap<String, String>,
+    validation: Validation,
+  ) -> Result<Response, u16> {
+    // Expecting session from an outer SecureResponder
+    match validation {
+      Some(dyn_box) => match dyn_box.downcast::<Session>() {
+        Ok(session_box) => match &mut request.message_body {
+          Some(body_reader) => match serde_json::from_reader::<_, DeleteDeckForm>(body_reader) {
+            Ok(form) => {
+              match self
+                .flash_manager
+                .delete_deck(session_box.as_ref(), form.deck_id)
+              {
+                Ok(()) => {
+                  let responder = StaticResponder::from_standard_code(200);
+                  return responder.build_response(request, params, None);
+                }
+                Err(_err) => {
+                  // TODO: Handle session errors / database errors                {
+                  return Err(500);
+                }
+              }
+            }
+            Err(_err) => return Err(400), // bad request
           },
           None => return Err(400),
         },
