b85261b55cc5c2d5927c9b6eeb8ef1c9f69aa8c4
WebeWizard <webewizard@gmail.com>
2020-01-15 15:45:05 -0700

    start working on responder for deck details (including list of cards

--- src/http/deck.rs
+++ src/http/deck.rs
@@ -9,6 +9,13 @@ use webe_web::validation::Validation;
 
 use std::collections::HashMap;
 
+// Form for selecting a single deck
+#[derive(Deserialize)]
+pub struct DeckIdForm {
+  #[serde(deserialize_with = "webe_auth::utility::deserialize_from_string")]
+  deck_id: u64,
+}
+
 // FETCH DECKS FOR ACCOUNT
 pub struct DecksResponder<'f> {
   flash_manager: &'f FlashManager<'f>,
@@ -23,6 +30,61 @@ impl<'f> DecksResponder<'f> {
 }
 
 impl<'f> Responder for DecksResponder<'f> {
+  fn build_response(
+    &self,
+    request: &mut Request,
+    params: &HashMap<String, String>,
+    validation: Validation,
+  ) -> Result<Response, u16> {
+    // Expecting session from an outer SecureResponder
+    match validation {
+      // TODO: maybe create some convenience function for unwrapping validation and parsing form from reader
+      Some(dyn_box) => match dyn_box.downcast::<Session>() {
+        Ok(session_box) => match self
+          .flash_manager
+          .get_decks_for_session(session_box.as_ref())
+        {
+          Ok(decks) => match serde_json::to_string(&decks) {
+            Ok(deck_text) => {
+              let responder = StaticResponder::new(200, deck_text);
+              return responder.build_response(request, params, None);
+            }
+            Err(_err) => {
+              dbg!(_err);
+              return Err(500);
+            } // TODO: parse session error, not found error, etc
+          },
+          Err(_err) => {
+            println!("manager error");
+            dbg!(session_box);
+            return Err(500);
+          }
+        },
+        Err(_err) => {
+          println!("session error");
+          dbg!(_err);
+          return Err(500);
+        }
+      },
+      None => return Err(400),
+    }
+  }
+}
+
+// FETCH DECKS FOR ACCOUNT
+pub struct DeckDetailsResponder<'f> {
+  flash_manager: &'f FlashManager<'f>,
+}
+
+impl<'f> DeckDetailsResponder<'f> {
+  pub fn new(flash_manager: &'f FlashManager<'f>) -> DeckDetailsResponder<'f> {
+    DeckDetailsResponder {
+      flash_manager: flash_manager,
+    }
+  }
+}
+
+impl<'f> Responder for DeckDetailsResponder<'f> {
   fn build_response(
     &self,
     request: &mut Request,
@@ -184,11 +246,6 @@ impl<'f> Responder for UpdateDeckResponder<'f> {
 }
 
 // DELETE DECK
-#[derive(Deserialize)]
-pub struct DeleteDeckForm {
-  #[serde(deserialize_with = "webe_auth::utility::deserialize_from_string")]
-  deck_id: u64,
-}
 
 pub struct DeleteDeckResponder<'f> {
   flash_manager: &'f FlashManager<'f>,
@@ -213,7 +270,7 @@ impl<'f> Responder for DeleteDeckResponder<'f> {
     match validation {
       Some(dyn_box) => match dyn_box.downcast::<Session>() {
         Ok(session_box) => match &mut request.message_body {
-          Some(body_reader) => match serde_json::from_reader::<_, DeleteDeckForm>(body_reader) {
+          Some(body_reader) => match serde_json::from_reader::<_, DeckIdForm>(body_reader) {
             Ok(form) => {
               match self
                 .flash_manager
